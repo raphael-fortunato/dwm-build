08284159d47a7f5a2e45d83a9e7ef17b2012c82d
4170512 <r.furtunato@students.uu.nl>
2021-09-27 20:15:47 +0200

    merge

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "UbuntuMono Nerd Font Mono:size=14 " };
-static const char dmenufont[]       = "UbuntuMono Nerd Font Mono:size=14 ";
+static const char *fonts[]          = { "UbuntuMono Nerd Font Mono:size=14", "Mononoki Nerd Font:pixelsize=16" ,"monospace:size=14"};
+static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#1E1E1E";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
