6b74fe5afc3db0d6637f6a5577e7ba4fb952f098
4170512 <r.furtunato@students.uu.nl>
2021-10-11 22:08:26 +0200

    merge

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const unsigned int gappiv    = 10;       /* vert inner gap between window
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "UbuntuMono Nerd Font Mono:size=14 ", "Mononoki Nerd Font:pixelsize=16" ,"monospace:size=14"};
